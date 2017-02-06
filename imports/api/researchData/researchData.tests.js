ee0389a2517b64a19f2c66bcc00ddd06e77196a3
B-raw <michaelbarry1989@hotmail.com>
2017-02-06 09:56:43 +0000

    add chimp

---
+++ imports/api/researchData/researchData.tests.js
@@ -0,0 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { expect } from 'meteor/practicalmeteor:chai';
+import { be } from 'meteor/practicalmeteor:chai';
+import { assert } from 'meteor/practicalmeteor:chai';
+import { resetDatabase } from 'meteor/xolvio:cleaner';
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import './accounts-server-onCreateUser.js'
+import '../imports/api/researchData/researchData.js'
+
 
 Meteor.startup(() => {
   // code to run on server at startup
